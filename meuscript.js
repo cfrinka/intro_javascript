d42b787e862699da1f16a2e0bfeb1947cd083aea
Carlos Frinka Neto <c.frinkaneto@gmail.com>
2020-06-17 21:37:57 -0300

    acrescentei a comparação de número iguais

--- meuscript.js
+++ meuscript.js
@@ -2,7 +2,9 @@ let num1 = Number(window.prompt('Insira o número 1'));
 let num2 = Number(window.prompt('Insira o número 2'));
 
 if (num1 > num2) {
-    alert(num1)
+    alert('Esse número é maior:' + num1)
+} else if (num1 == num2) {
+    alert('os dois números são iguais')
 } else {
-    alert(num2)
+    alert('Esse número é maior:' + num2)
 }
